86d904560a699e3d17ee77274374ac0e85a15c0f
Cuong Le <cle15@lsu.edu>
2016-10-04 08:19:45 -0500

    what

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var robot;
 var skulls;
+var aliens;
 
 function preload()
 {
@@ -11,11 +12,12 @@ function setup()
 {
 createCanvas(640,500);
 background(100, 50, 40);
-image(skulls);
+//image(skulls);
 }
 function draw()
 {
-
+image(skulls,0,0);
+image(skulls,0,mouseY * -1);
 if(mouseIsPressed)
 {
   image(skulls, random(width), random (height),random(width), random (height) );
